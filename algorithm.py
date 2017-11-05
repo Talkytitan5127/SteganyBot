3da2f1a12a990d34fb88dd407628a087371c4eac
Talkytitan5127 <pachagorbunoff@yandex.ru>
2017-11-05 19:54:13 +0300

    check code with pep8

--- algorithm.py
+++ algorithm.py
@@ -2,12 +2,14 @@ from PIL import Image, ImageDraw
 from bitarray import bitarray
 import sys
 
+
 class Picture:
     def __init__(self):
         self.image = None
         self.width = 0
         self.height = 0
         self.draw = None
+
     def __init__(self, path):
         self.image = Image.open(path)
         self.width = self.image.size[0]
@@ -17,93 +19,72 @@ class Picture:
     def GetBit(self):
         pix = self.image.load()
         bText = bitarray()
-        lenText = pix[0,0][2]
+        lenText = pix[0, 0][2]
         print("length => ", lenText)
-        
         i = 4
         j = 4
         Count = 0
         t = 0
         result = 0
-        
         while(Count < lenText):
-            
             bitpix = 0
             for k in range(1, 4):
                 try:
-                    bitpix += pix[i,j + k][2]
-                except:
+                    bitpix += pix[i, j + k][2]
+                except Exception:
                     pass
                 try:
-                    bitpix += pix[i,j - k][2]
-                except:
+                    bitpix += pix[i, j - k][2]
+                except Exception:
                     pass
                 try:
-                    bitpix += pix[i + k,j][2]
-                except:
+                    bitpix += pix[i + k, j][2]
+                except Exception:
                     pass
                 try:
-                    bitpix += pix[i - k,j][2]
-                except:
+                    bitpix += pix[i - k, j][2]
+                except Exception:
                     pass
-            
             value = int(bitpix / (4 * 3))
-            
             result += pix[i, j][2] - value
-            
             t += 1
-
             j += 4
             if (j >= self.height):
                 j = 0
                 i += 4
-
             if t != 2:
                 continue
-            
             result = int(result / 2)
-
             if (result == 0 and pix[i, j][2] == 255):
                 result = 0.5
             if (result == 0 and pix[i, j][2] == 0):
                 result = -0.5
-                                
             if result > 0:
                 bText += '1'
             else:
                 bText += '0'
-
-            t = 0        
+            t = 0
             Count += 1
             result = 0
-
         return bText
-
 
     def ChangePix(self, text):
         pix = self.image.load()
-        
         k = 0
         i = 4
         j = 4
         t = 1
-        
-        self.draw.point((0, 0),(pix[0, 0][0],
-            pix[0, 0][1], len(text)))
-        
+        self.draw.point((0, 0), (pix[0, 0][0],
+                        pix[0, 0][1], len(text)))
         while k < len(text):
-    
             red = pix[i, j][0]
             green = pix[i, j][1]
             blue = pix[i, j][2]
-            lam = 0.3*red + 0.59*green + 0.11*blue       
+            lam = 0.3*red + 0.59*green + 0.11*blue
             xu = 0.5
             alpha = 2
-
             if lam == 0:
                 lam = alpha/xu
-
-
             if text[k]:
                 blue += lam*xu
                 blue = int(blue) + 1
@@ -113,31 +94,24 @@ class Picture:
                 blue -= lam*xu
                 if blue < 0:
                     blue = 0
-    
             blue = int(blue)
             self.draw.point((i, j), (red, green, blue))
-            
             j += 4
             if (j >= self.height):
                 j = 4
                 i += 4
-            
             if t == 2:
                 k += 1
                 t = 0
-
             t += 1
-
         return "Success encryption"
 
-    
     def WritePix(self, flag):
         file = flag + "Crypt.txt"
         data = self.image.getdata()
         with open(file, 'w') as f:
             for i in data:
                 print(i, file=f)
-    
 
     def SaveChange(self, path):
         newpath = path.replace("jpg", "png")
@@ -148,13 +122,12 @@ class Picture:
         return newpath
 
 
-
 class Container:
     def init(self):
         self.MyImage = Picture()
         self.Pathfile = None
         self.UserName = None
-    
+
     def SetModule(self, path):
         self.Pathfile = path
         self.MyImage = Picture(self.Pathfile)
@@ -166,11 +139,8 @@ class Container:
         bText = bitarray()
         bText.frombytes(text.encode('utf-8'))
         print(bText, len(bText))
-        
         self.MyImage.WritePix(flag="Before")
-                
         print(self.MyImage.ChangePix(text=bText))
-
         self.MyImage.WritePix(flag="After")
 
     def Save(self):
@@ -190,7 +160,7 @@ class Container:
             result = bText.decode('utf-8')
             print('result => ', result)
             return result
-        except:
+        except Exception:
             return "sorry, there are undecoded byte in decrypt message"
 
 
@@ -209,3 +179,4 @@ if __name__ == '__main__':
         obj.SetModule("photos/decrypt/photo3.png")
         text = obj.Decrypt()
         print(text)
+
--- main.py
+++ main.py
@@ -12,9 +12,11 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, File
+from telegram import KeyboardButton, ReplyKeyboardMarkup
+from telegram import ReplyKeyboardRemove, File
 from telegram.ext import Updater, CommandHandler
-from telegram.ext import MessageHandler, Filters, Dispatcher, CallbackQueryHandler, RegexHandler
+from telegram.ext import MessageHandler, Filters, Dispatcher
+from telegram.ext import CallbackQueryHandler, RegexHandler
 import logging
 
 import Strings
@@ -23,7 +25,8 @@ from random import randint
 from algorithm import *
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+logging.basicConfig(format="""%(asctime)s - %(name)s -
+                        %(levelname)s - %(message)s""",
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
@@ -37,21 +40,17 @@ class SteganyBot:
         self.flagEncrypt = False
         self.flagDecrypt = False
 
-
     def Start(self, bot, update):
         logger.info("Method start")
         update.message.reply_text(text=Strings.greeting)
 
         keyboard = [[KeyboardButton("EncryptMessage", callback_data='1')],
-                 [KeyboardButton("DecryptMessage", callback_data='2')],
-                [KeyboardButton("Option 3", callback_data='3')]
-        ]
-        
+                    [KeyboardButton("DecryptMessage", callback_data='2')],
+                    [KeyboardButton("Option 3", callback_data='3')]
+                    ]
         reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
         update.message.reply_text('Please choose:', reply_markup=reply_markup)
-        
         return logger.info("Done start")
-
 
     def Choose(self, bot, update):
         logger.info("Method \'Choose\'")
@@ -65,16 +64,13 @@ class SteganyBot:
             self.AskPicture(bot, update)
             logger.info("choose to decrypt")
 
-
     def EncryptMessage(self, bot, update, letter):
         logger.info("\'EncryptMessage\'")
 
         if not self.flagEncrypt:
             update.message.reply_text(update.message.text)
             logger.info("message was sent")
-            
             return "Not yet"
-        
         obj = Container()
         rang = len(listdir("photos/encrypt/"))
         photo_id = str(randint(1, rang))
@@ -83,39 +79,29 @@ class SteganyBot:
         obj.SetUser(update.message.chat.username)
 
         logger.info(obj.Encrypt(letter))
-        
         filepath = obj.Save()
         print("filepath => ", filepath)
         update.message.reply_text("Successful! Get the picture")
         update.message.reply_document(open(filepath, "rb"))
-        
         self.flagEncrypt = False
-        
         return logger.info("success encryption")
-
 
     def DecryptMessage(self, bot, update, number):
         logger.info("DecryptMessage")
 
         obj = Container()
-        
         path = "photos/fromUser/photo"+str(number)+".png"
         print(path)
-        
         obj.SetModule(path)
         text = obj.Decrypt()
         answer = "extracted message => " + text
         update.message.reply_text(answer)
-        
         self.flagDecrypt = False
-        
         return logger.info("decryption method success")
-
 
     def Help(self, bot, update):
         logger.info("Help")
         update.message.reply_text(Strings.helpString)
-
 
     def AnswerLog(self, bot, update):
         logger.info("Log method")
@@ -123,86 +109,82 @@ class SteganyBot:
         ID = update.message.chat.id
         Answer = "Me writing from chat: " + str(ID)
         Answer += "\nUser => " + str(update.message.chat.username)
-        Answer += "\nFull Name => " + str(update.message.chat.first_name) + " " + str(update.message.chat.last_name)
-        
+        Answer += "\nFull Name => " + str(update.message.chat.first_name)
+        Answer += " " + str(update.message.chat.last_name)
         bot.send_message(text=Answer, chat_id=243247509)
-        
         return logger.info("log was sent")
-
 
     def GetText(self, bot, update):
         logger.info("GetText")
-        
         self.EncryptMessage(bot, update, letter=update.message.text)
         self.AnswerLog(bot, update)
-        
         return logger.info("GetText done")
-
 
     def GetDoc(self, bot, update):
         logger.info("\'getPhoto\'")
-        
         user = update.message.chat.username
         photo_file = bot.get_file(update.message.document.file_id)
 
         num = len(listdir("photos/fromUser/")) + 1
         path = "photos/fromUser/photo" + str(num) + ".png"
         photo_file.download(path)
-        
         if (len(listdir("photos/fromUser/")) > 5):
             for i in listdir("photos/fromUser/"):
                 remove("photos/fromUser/" + i)
-
         print(self.DecryptMessage(bot, update, number=num))
         return logger.info("GetDoc success")
 
-
     def GetPhoto(self, bot, update):
         logger.info("\'getPhoto\'")
-
         user = update.message.chat.username
         photo_file = bot.get_file(update.message.photo[-1].file_id)
-       
         num = len(listdir("photos/fromUser/")) + 1
         path = "photos/fromUser/photo" + str(num) + ".png"
         photo_file.download(path)
-
         if (len(listdir("photos/fromUser/")) > 5):
             for i in listdir("photos/fromUser/"):
                 remove("photos/fromUser/" + i)
-        
         print(self.DecryptMessage(bot, update, number=num))
-
 
     def Error(self, bot, update, error):
         logger.warn('Update "%s" caused error "%s"' % (update, error))
 
-
     def AskText(self, bot, update):
         update.message.reply_text("Send me a message you want to encrypt")
-
 
     def AskPicture(self, bot, update):
         update.message.reply_text("Please, upload the picture to decrypt")
 
-
     def AddHandler(self):
-        self.dispatcher.add_handler(CommandHandler("start", self.Start))
+        self.dispatcher.add_handler(CommandHandler(
+            "start", self.Start)
+        )
 
-        self.dispatcher.add_handler(RegexHandler('^((E|e)ncrypt)|((D|d)ecrypt)',self.Choose))
+        self.dispatcher.add_handler(RegexHandler(
+            '^((E|e)ncrypt)|((D|d)ecrypt)', self.Choose)
+        )
 
-        self.dispatcher.add_handler(CommandHandler("help", self.Help))
+        self.dispatcher.add_handler(CommandHandler(
+            "help", self.Help)
+        )
 
-        self.dispatcher.add_handler(MessageHandler(Filters.text, self.GetText))
+        self.dispatcher.add_handler(MessageHandler(
+            Filters.text, self.GetText)
+        )
 
-        self.dispatcher.add_handler(MessageHandler(Filters.document, self.GetDoc))
-        
-        self.dispatcher.add_handler(MessageHandler(Filters.photo, self.GetPhoto))
+        self.dispatcher.add_handler(MessageHandler(
+            Filters.document, self.GetDoc)
+        )
 
-        self.dispatcher.add_handler(CommandHandler("Log", self.AnswerLog))
+        self.dispatcher.add_handler(MessageHandler(
+            Filters.photo, self.GetPhoto)
+        )
+
+        self.dispatcher.add_handler(CommandHandler(
+            "Log", self.AnswerLog)
+        )
 
         self.dispatcher.add_error_handler(self.Error)
-
 
     def StartBot(self):
         self.AddHandler()
@@ -211,7 +193,7 @@ class SteganyBot:
 
 
 if __name__ == '__main__':
-    token = TOKEN
+    token = "TOKEN"
     bot = SteganyBot(token)
     bot.StartBot()
-    
+
